fbfdc6edc23f593389f758441cd65da79f2dab8a
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-18 20:10:50 +0200

    Initial commit

--- ol-006-10/ContentView.swift
+++ ol-006-10/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        Picker("Select you student", selection: $selectedStudent) {
+            ForEach(0 ..< students.count) {
+                Text(self.students[$0])
+            }
+        }
+        
+//        Form {
+//            ForEach(0..<100) {
+//                Text("Number \($0)")
+//            }
+//        }
     }
 }
 
